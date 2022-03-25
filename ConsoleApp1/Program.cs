9cc2fd3c4836ea313d7e01afb3f2c5e65b5d5a39
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2022-03-25 11:13:51 +0530

    hello1
    hello1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var hello = "Hello World";
+            var hello1 = "Hello World";
             
         }
     }
