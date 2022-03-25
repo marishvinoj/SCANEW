066a9214c87c653bf8542ae8f17aa9ee4241db20
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2022-03-25 11:09:11 +0530

    Hello World test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var hello = "Hello World";
+            
         }
     }
 }
